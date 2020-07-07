aa82a897f0ed050386c9f3425dd3ea85fb91627a
Federico Guerinoni <guerinoni.federico@gmail.com>
2020-07-07 16:56:41 +0200

    Add test for extension without dot
    If you use extension without prefix dot.
    
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- replacer_test.go
+++ replacer_test.go
@@ -5,13 +5,28 @@ import (
 	"testing"
 )
 
-func TestExecChangeExtension(t *testing.T) {
+func TestExecChangeExtensionWithDot(t *testing.T) {
 	file, err := os.Create("foo.txt")
 	if err != nil {
 		t.Errorf("error creating file")
 	}
 
 	execChangeExtension(file.Name(), ".txt", ".c")
+
+	if _, err := os.Stat("foo.c"); os.IsNotExist(err) {
+		t.Errorf("file not exists after change extension")
+	}
+
+	os.Remove("foo.c")
+}
+
+func TestExecChangeExtensionWithoutDot(t *testing.T) {
+	file, err := os.Create("foo.txt")
+	if err != nil {
+		t.Errorf("error creating file")
+	}
+
+	execChangeExtension(file.Name(), "txt", "c")
 
 	if _, err := os.Stat("foo.c"); os.IsNotExist(err) {
 		t.Errorf("file not exists after change extension")
